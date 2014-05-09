a2f3bca952784bc2e9016d5b6cd3cfed2ab2b07c
Minh-Tue Vo <minhtuev@mit.edu>
2014-05-09 17:40:37 -0400

    today work

--- datetime_extractor.py
+++ datetime_extractor.py
@@ -3,13 +3,21 @@ from os.path import isfile, join
 import re
 import datetime
 from dateutil import parser
+import string
 
-def get_gps_text_files():
-    mypath = "."
-    onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
-    pattern = re.compile("2014")
-    onlyfiles = [ f for f in onlyfiles if pattern.match(f)]
+def get_files_from_directory(path, pattern):
+    onlyfiles = [ f for f in listdir(path) if isfile(join(path,f)) and pattern.match(f)]
     return onlyfiles
+    
+def get_gps_text_files():
+    pattern = re.compile("2014[0-9]*.txt")
+    textfiles = get_files_from_directory(".", pattern)
+    return textfiles
+
+def get_video_files():
+    pattern = re.compile("2014[0-9\_]*.avi")
+    videofiles = get_files_from_directory(".", pattern)
+    return videofiles
 
 def read_gps_text_file(filename):
     f = open(filename, 'r')
@@ -19,6 +27,7 @@ def read_gps_text_file(filename):
     while line != '':
         line = str(line).strip().split(',')
         date_object = parser.parse(line[0])
+        date_object -= datetime.timedelta(hours = 4)
         # remove noises
         if (date_object.year == 2014):
             result.append((date_object, float(line[1]), float(line[2])))
@@ -39,7 +48,14 @@ def get_deltas():
         print gps_data[i+1], gps_data[i]
 
 def get_video_datetime(filename):
-    pass
+    filename = filename.split(".")[0]
+    filename = filename.split("_")
+    filename = string.join(filename[:len(filename) - 1], "-")
+    date_object = parser.parse(filename)
+    return date_object
 
-
+print get_gps_text_files()
+video_files = get_video_files()
+print video_files
+print get_video_datetime(video_files[0])
 # print get_all_gps_data()
--- video_extractor.py
+++ video_extractor.py
@@ -1,34 +1,67 @@
 import cv2
+import os
+import pprint
 from datetime_extractor import *
 
 # get the length of the video in seconds
 def get_video_length(cam):
-    return cam.get(7)/cam.get(cv2.cv.CV_CAP_PROP_FPS)
+    return get_num_frames(cam)/get_frame_rate(cam)
 
-def process_video(filename, gps_list):
-    cam = cv2.VideoCapture(filename)
+def get_frame_rate(cam):
+    return cam.get(cv2.cv.CV_CAP_PROP_FPS)
+
+def get_num_frames(cam):
+    return cam.get(7)
+
+def print_video_properties(cam):
     print "Video Properties:"
     print "\t Width: ",cam.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
     print "\t Height: ",cam.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
     print "\t FourCC: ",cam.get(cv2.cv.CV_CAP_PROP_FOURCC)
-    print "\t Framerate: ",cam.get(cv2.cv.CV_CAP_PROP_FPS)
-    print "\t Number of Frames: ",cam.get(7)
+    print "\t Framerate: ",get_frame_rate(cam)
+    print "\t Number of Frames: ",get_num_frames(cam)
+
+def process_video(filename, gps_list):
+    cam = cv2.VideoCapture(filename)
+    print_video_properties(cam)
+    # video length in seconds
     video_length = get_video_length(cam)
     video_datetime = get_video_datetime(filename)
 
-    print "videolength", video_length
-    print video_datetime
-    # video_datetime_end = video_datetime + video_length
-    # gps_data = get_all_gps_data()
-    # frame_num = 0
-    # # iterating through all (datetime, lat, lon)
-    # for (dt, lat, lon) in gps_data:
-    #     if (video_datetime <= dt) and (dt <= video_datetime_end):
-    #         print dt
+    folder_name = filename.split(".")[0]
+    # if the folder does not exist, make it
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
 
+    print "videolength", video_length
+    start_time = video_datetime
+    end_time = (video_datetime + datetime.timedelta(seconds = video_length))
+    print "video start time:", start_time.time()
+    print "video end time:", end_time.time()
+    
+    frame_num = 1
+    frame_time = 0
+    frame_rate = get_frame_rate(cam)
+    max_frame_num = get_num_frames(cam)
+
+    # iterating through all (datetime, lat, lon) and find the proper time interval
+    for (dt, lat, lon) in gps_list:
+        if (start_time.time() <= dt.time()) and (dt.time() <= end_time.time()):
+            frame_time = start_time + datetime.timedelta(seconds = frame_num*1.0/get_frame_rate(cam))
+            # navigating toward the exact time
+            while abs((frame_time - dt).seconds) > 1 and frame_num <= max_frame_num:
+                success, image = vidcap.read()
+                frame_num += 1
+                frame_time = start_time + datetime.timedelta(seconds = frame_num*1.0/get_frame_rate(cam))
+            # while at the exact time, extrac the frame and save
+            while abs((frame_time - dt).seconds) < 1 and frame_num <= max_frame_num:
+                cv2.imwrite(folder_name + ("/frame%d.jpg" % frame_num), image)
+                success, image = vidcap.read()
+                frame_num += 1
+                frame_time = start_time + datetime.timedelta(seconds = frame_num*1.0/get_frame_rate(cam))        
     cam.release()
 
-filenames = get_gps_text_files()
+filenames = get_video_files()
 gps_data = get_all_gps_data()
 
 process_video(filenames[0], gps_data)
